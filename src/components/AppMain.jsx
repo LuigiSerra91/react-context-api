871c6fce1ce573f2f5e8a22509f0dbcb050723d9
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-11-27 12:26:44 +0100

    fix-bug

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Children } from 'react';
+import React, { useState, useEffect} from 'react';
 import AddCanvas from "./AddCanvas";
 import AppCard from "./AppCard";
 import PostList from './PostList'
